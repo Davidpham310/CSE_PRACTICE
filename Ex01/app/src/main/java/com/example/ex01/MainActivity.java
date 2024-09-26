8b6d47e6a73cdebfcbc3c4dc87905ef63dbc1489
Trường Phạm <105166852+Davidpham310@users.noreply.github.com>
2024-09-26 23:20:34 +0700

    Complete Ex01

--- Ex01/app/src/main/java/com/example/ex01/MainActivity.java
+++ Ex01/app/src/main/java/com/example/ex01/MainActivity.java
@@ -1,20 +1,37 @@
 package com.example.ex01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editA , editB , editSum ;
+    Button btnSum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        editA = findViewById(R.id.editA);
+        editB = findViewById(R.id.editB);
+        editSum = findViewById(R.id.editSum);
+        btnSum = findViewById(R.id.btnSum);
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!editA.getText().toString().isEmpty() && !editB.getText().toString().isEmpty()){
+                    int sum = Integer.parseInt(editA.getText().toString()) + Integer.parseInt(editB.getText().toString());
+                    editSum.setText(String.valueOf(sum));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Nhập số A và B không được rỗng", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
