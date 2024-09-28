62bf788c464e0a52bc5f35e464a85c8c1b40e968
Trường Phạm <105166852+Davidpham310@users.noreply.github.com>
2024-09-29 00:57:52 +0700

    Complete Ex05

--- Ex05/app/src/main/java/com/example/ex05/MainActivity.java
+++ Ex05/app/src/main/java/com/example/ex05/MainActivity.java
@@ -1,24 +1,64 @@
 package com.example.ex05;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editName , editHeight , editWeight , editBMI , editDiagnosis;
+    Button btnBMI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Map();
+        Event();
+    }
+
+    private void Event() {
+        btnBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!editWeight.getText().toString().isEmpty() && !editHeight.getText().toString().isEmpty() && !editName.getText().toString().isEmpty()) {
+                    double weight = Double.parseDouble(editWeight.getText().toString());
+                    double height = Double.parseDouble(editHeight.getText().toString());
+                    double result = weight / (height * height);
+                    String bmi;
+                    if (result > 35) {
+                        bmi = "người béo phì độ III";
+                    } else if (result >= 30) {
+                        bmi = "người béo phì độ II";
+                    } else if (result >= 25) {
+                        bmi = "người béo phì độ I";
+                    } else if (result >= 18) {
+                        bmi = "người bình thường";
+                    } else {
+                        bmi = "người gầy";
+                    }
+                    editBMI.setText(String.format(Locale.UK, "%.2f", result));
+                    editDiagnosis.setText(editName.getText().toString() + "_" + bmi);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Vui lòng điền đầy đủ", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+    }
+
+    private void Map() {
+        editName = findViewById(R.id.editName);
+        editHeight = findViewById(R.id.editHeight);
+        editWeight = findViewById(R.id.editWeight);
+        editBMI = findViewById(R.id.editBMI);
+        editDiagnosis = findViewById(R.id.editDiagnosis);
+        btnBMI = findViewById(R.id.btnBMI);
     }
 }
