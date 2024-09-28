3e942b61ab938827d10b20e5da081235ab960b98
Trường Phạm <105166852+Davidpham310@users.noreply.github.com>
2024-09-28 15:52:18 +0700

    complete Ex04

--- Ex04/app/src/main/java/com/example/ex04/MainActivity.java
+++ Ex04/app/src/main/java/com/example/ex04/MainActivity.java
@@ -1,24 +1,80 @@
 package com.example.ex04;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editC , editF;
+    Button btnConverttoC , btnConverttoF , btnClear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Map();
+        Event();
+    }
+
+    private void Event() {
+        btnConverttoC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!editF.getText().toString().isEmpty() && editC.getText().toString().isEmpty() || !editF.getText().toString().isEmpty()  && !editC.getText().toString().isEmpty()){
+                    Convert(editF , "toC");
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Nhiệt độ F không được rỗng", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+        btnConverttoF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!editC.getText().toString().isEmpty()  && editF.getText().toString().isEmpty() || !editC.getText().toString().isEmpty()  && !editF.getText().toString().isEmpty() ){
+                    Convert(editC , "toF");
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Nhiệt độ C không được rỗng", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editC.setText("");
+                editF.setText("");
+            }
+        });
+    }
+
+    private void Convert(EditText editText, String str) {
+        double result;
+        double i = Double.parseDouble(editText.getText().toString());
+        switch (str){
+            case "toC":
+                result = (i - 32) * 5/9;
+                editC.setText(String.format(Locale.US ,"%.2f" , result));
+                break;
+            case "toF":
+                result = i * 9/5 + 32;
+                editF.setText(String.format(Locale.US , "%.2f" , result));
+                break;
+        }
+    }
+
+    private void Map() {
+        editC = findViewById(R.id.editC);
+        editF = findViewById(R.id.editF);
+        btnConverttoC = findViewById(R.id.btnConverttoC);
+        btnConverttoF = findViewById(R.id.btnConverttoF);
+        btnClear = findViewById(R.id.btnClear);
     }
 }
