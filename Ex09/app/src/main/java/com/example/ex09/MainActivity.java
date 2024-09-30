feff2d4e9459d246e3ceb9a21eb85e0c4e0fd62f
Trường Phạm <105166852+Davidpham310@users.noreply.github.com>
2024-09-30 12:58:03 +0700

    Complete Ex09

--- Ex09/app/src/main/java/com/example/ex09/MainActivity.java
+++ Ex09/app/src/main/java/com/example/ex09/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex09;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btnMain = findViewById(R.id.btnMain);
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , MainActivity2.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- Ex09/app/src/main/java/com/example/ex09/MainActivity2.java
+++ Ex09/app/src/main/java/com/example/ex09/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.example.ex09;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,14 @@ public class MainActivity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btnChild = findViewById(R.id.btnChild);
+        btnChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this , MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
