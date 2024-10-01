ff3ea2499f5fc83273241cc563b13740b5b776c2
Trường Phạm <105166852+Davidpham310@users.noreply.github.com>
2024-10-02 00:12:45 +0700

    Complete Ex14

--- Ex14/app/src/main/java/com/example/ex14/MainActivity.java
+++ Ex14/app/src/main/java/com/example/ex14/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.ex14;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
+import android.Manifest;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        if(ActivityCompat.checkSelfPermission(MainActivity.this ,
+                Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS} ,1);
+            return;
+        }
     }
 }
---
+++ Ex14/app/src/main/java/com/example/ex14/MySmsReceive.java
@@ -0,0 +1,33 @@
+package com.example.ex14;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class MySmsReceive extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        processReceive(context , intent);
+    }
+
+    private void processReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        String message = "";
+        String body = "";
+        String address = "";
+        if (extras != null){
+            Object[] smsEtra = (Object[]) extras.get("pdus");
+            for (int i = 0 ; i < smsEtra.length ; i++){
+                SmsMessage sms = SmsMessage.createFromPdu((byte[])smsEtra[i]);
+                body = sms.getMessageBody();
+                address = sms.getOriginatingAddress();
+                message += "Có một tin nhắn từ" + address + "\n" + body + "vừa gửi tới";
+            }
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
