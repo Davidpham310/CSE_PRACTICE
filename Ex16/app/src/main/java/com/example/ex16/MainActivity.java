c479db6f5a100dfea67f9915e06dcce40889e718
Trường Phạm <105166852+Davidpham310@users.noreply.github.com>
2024-10-02 21:04:12 +0700

    Create Ex16

---
+++ Ex16/app/src/main/java/com/example/ex16/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ex16;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textView = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
+        final  String[] arr = {"Nokia 1280" , "Sam Sung J6" , "Iphone 16" , "Sony" , "HTC"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_main , arr);
+        listView.setAdapter(adapter);
+    }
+}
